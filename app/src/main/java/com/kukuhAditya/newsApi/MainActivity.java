32e4b068945e1f1c425f8cb1019055e5ce873f74
Kukuh Aditya <kukuhAditya3000@gmail.com>
2023-10-28 00:41:42 +0700

    Project Mobile

---
+++ app/src/main/java/com/kukuhAditya/newsApi/MainActivity.java
@@ -0,0 +1,55 @@
+package com.kukuhAditya.newsApi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+
+import com.kukuhAditya.newsApi.Models.NewsApiResponse;
+import com.kukuhAditya.newsApi.Models.NewsHeadlines;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    CustomAdapter adapter;
+    ProgressDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Fetching news articles..");
+        dialog.show();
+
+        RequestManager manager = new RequestManager(this);
+        manager.getNewsHeadLines(listener, "general", null);
+
+    }
+
+    private final OnFetchDataListener<NewsApiResponse> listener = new OnFetchDataListener<NewsApiResponse>() {
+        @Override
+        public void onFetchData(List<NewsHeadlines> list, String message) {
+            showNews(list);
+            dialog.dismiss();
+        }
+
+        @Override
+        public void onError(String message) {
+
+        }
+    };
+
+    private void showNews(List<NewsHeadlines> list) {
+        recyclerView = findViewById(R.id.recycler_main);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        adapter = new CustomAdapter(this, list);
+        recyclerView.setAdapter(adapter);
+    }
+
+}
---
+++ app/src/main/java/com/kukuhAditya/newsApi/OnFetchDataListener.java
@@ -0,0 +1,10 @@
+package com.kukuhAditya.newsApi;
+
+import com.kukuhAditya.newsApi.Models.NewsHeadlines;
+
+import java.util.List;
+
+public interface OnFetchDataListener<NewsApiResponse>{
+    void onFetchData(List<NewsHeadlines> list, String message);
+    void onError(String message);
+}
